f647f19e52d64cf83bbd060311a4c50d937c6d4a
Tim Wöhrle <tim.woehrle3010@gmail.com>
2024-11-02 01:46:26 +0100

    feat: initialize Minestom server with command-line arguments

--- bootstrap/src/main/java/net/minestom/bootstrap/Main.java
+++ bootstrap/src/main/java/net/minestom/bootstrap/Main.java
@@ -1,8 +1,58 @@
 package net.minestom.bootstrap;
 
+import dev.nipafx.args.Args;
+import dev.nipafx.args.ArgsParseException;
+import net.linelabs.extensions.ExtensionCommand;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.ServerFlag;
+import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.extras.velocity.VelocityProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws ArgsParseException {
+        log.debug("Creating Minestom Server with Args: {}", Arrays.toString(args));
+        ServerArgs serverArgs = Args.parse(args, ServerArgs.class);
+        createServer(serverArgs);
+    }
+
+    private static void createServer(ServerArgs serverArgs) {
+        MinecraftServer minecraftServer = MinecraftServer.init();
+
+        if (ServerFlag.EXTENSIONS_ENABLED) {
+            MinecraftServer.getCommandManager().register(new ExtensionCommand(MinecraftServer.getExtensionManager()));
+        }
+
+        boolean isOnlineMode = serverArgs.onlineMode().orElse(false);
+        if (isOnlineMode) {
+            log.info("Online mode enabled, skins will be render!");
+            MojangAuth.init();
+        }
+
+        String secret = serverArgs.velocitySecret().orElse(null);
+        if (secret != null) {
+            VelocityProxy.enable(secret);
+            log.info("Velocity proxy enabled with secret: {}", secret);
+        }
+
+        minecraftServer.start(serverArgs.host(), serverArgs.port().orElse(ServerArgs.DEFAULT_PORT));
+    }
+
+    private record ServerArgs(
+            String host,
+            Optional<Integer> port,
+            Optional<Boolean> onlineMode,
+            Optional<String> velocitySecret) {
+
+        private static final int DEFAULT_PORT = 25565;
+
     }
 
 }
